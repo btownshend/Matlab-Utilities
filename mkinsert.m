7c4b93c09d70cc9f365779b86f2486490db13ee9
Brent Townshend <bst@tc.com>
2013-06-04 16:19:58 -0700

    Added mkinsert.m setfig.m

---
+++ mkinsert.m
@@ -0,0 +1,42 @@
+% Save a figure for use in powerpoint or prezi
+function mkinsert(filename,fnum,aspect)
+  if nargin<2
+    fnum=gcf;
+  end
+  figure(fnum);
+  fontsize=16;
+  if nargin<3
+    %aspect=4/3;
+    aspect=16/9;
+  end
+  width=1280;   
+  height=width/aspect;
+  width=width-0.5;% Always end up with 1 extra pixel 
+  height=height-0.5;
+
+  ps=get(gcf,'position');
+  ps(3)=width;
+  ps(4)=height;
+  set(gcf,'position',ps);
+  set(gcf,'PaperUnits','inches');
+  resolution=72;
+  set(gcf,'PaperPosition',[0 0 width height]/resolution);
+  set(gcf,'PaperSize',[width height]/resolution);
+  axes=get(gcf,'children')
+  for j=1:length(axes)
+    set(axes(j),'FontSize',fontsize);
+    set(get(axes(j),'XLabel'),'FontSize',fontsize,'FontWeight','bold');
+    set(get(axes(j),'YLabel'),'FontSize',fontsize,'FontWeight','bold');
+    set(get(axes(j),'Title'),'FontSize',fontsize,'FontWeight','bold');
+    c=get(axes(j),'Children');
+    for i=1:length(c)
+      try
+        set(c(i),'MarkerSize',20);
+      catch me
+        fprintf('Ignoring exception\n');
+      end
+    end
+  end
+  print(gcf,'-dpng',['-r',num2str(72)],filename);
+  fprintf('Saved figure to %s\n', [filename,'.png']);
+end
---
+++ setfig.m
@@ -0,0 +1,24 @@
+% setfig - Create a new figure or reuse one with given name
+function setfig(s)
+global figlist;
+undef=0;
+eval('figlist.name;','undef=1;');
+if undef==1
+  figlist.name={s};
+  figlist.fignum=20;
+  figure(figlist.fignum);
+  set(figlist.fignum(end),'Name',figlist.name{1});
+  return;
+end
+for i=1:length(figlist.name)
+  if strcmp(figlist.name(i),s) == 1
+    figure(figlist.fignum(i));
+    set(figlist.fignum(i),'Name',figlist.name{i});
+    return;
+  end
+end
+figlist.name{end+1}=s;
+figure(max(figlist.fignum)+1);
+figlist.fignum=[figlist.fignum,gcf];
+set(figlist.fignum(end),'Name',figlist.name{end});
+return;
