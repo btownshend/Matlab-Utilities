d3f7af481f975db1e7a5e9584dba520178c20123
Brent Townshend <bst@tc.com>
2014-04-16 23:34:32 -0700

    Added holdfig.m holdallfigs.m

---
+++ holdfig.m
@@ -0,0 +1,13 @@
+% holdfig
+% Used in conjuction with savefig to prevent a figure window from being reused
+function holdfig(fnum)
+global figlist
+sel=[figlist.fignum]==fnum;
+if isempty(sel)
+  figlist.name{end+1}=sprintf('HOLD%d',fnum);
+  figlist.fignum{end+1}=fnum;
+else
+  fprintf('Held figure %d: %s\n', fnum, figlist.name{sel});
+  figlist.name{sel}=[figlist.name{sel},'-',sprintf('HOLD%d',fnum)];
+end
+
