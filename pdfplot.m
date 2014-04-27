6870d579f60e6452297ddaa876725c6812ded813
Brent Townshend <bst@tc.com>
2014-04-27 00:49:17 -0700

    Added log option to pdfplot

--- pdfplot.m
+++ pdfplot.m
@@ -9,10 +9,12 @@ end
 if nargin<4
   nbins=max(2,min([ceil(length(x)/10), 200]));
 end
-if (mean(x<=0) > 0.01)
+if (mean(x<=0) > 0.01 & dolog)
   fprintf('pdfplot Warning: %.1f%% (%d points) of data is non-positive and log plotting was requested.\n', 100*mean(x<=0),sum(x<=0));
 end
-x=x(x>0);
+if dolog
+  x=x(x>0);
+end
 xs=sort(x);
 first=1;
 px=[]; py=[];
