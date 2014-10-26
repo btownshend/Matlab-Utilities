7236e8fe613d29d914e7cea4361070d25d43a7f4
Brent Townshend <bst@tc.com>
2014-10-26 00:23:06 -0700

    Handle new non-numeric figure handles

--- holdfig.m
+++ holdfig.m
@@ -2,6 +2,9 @@
 % Used in conjuction with savefig to prevent a figure window from being reused
 function holdfig(fnum)
 global figlist
+if ~isnumeric(fnum)
+  fnum=get(fnum,'Number');
+end
 sel=[figlist.fignum]==fnum;
 if isempty(sel)
   figlist.name{end+1}=sprintf('HOLD%d',fnum);
