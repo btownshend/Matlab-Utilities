d95f99b19d2141414e7b7e0eb4754ece9ec7f457
Brent Townshend <bst@tc.com>
2014-12-10 14:56:06 -0800

    Added display of valid options

--- processargs.m
+++ processargs.m
@@ -6,7 +6,14 @@ function args=processargs(args,va)
 i=1;
 while i<=length(va)
   if ~isfield(args,va{i})
-    error('Unknown option: "%s"\n',va{i});
+    s=sprintf('Unknown option: "%s"\nValid options are:',va{i});
+    fn=fieldnames(args);
+    for i=1:length(fn)
+      if ~strncmp(fn{i},'SET',3)
+        s=[s,sprintf('%s ',fn{i})];
+      end
+    end
+    error(s);
   end
   args.(['SET',va{i}])=true;   % Flag that it was set explicitly
   if islogical(args.(va{i})) && (i==length(va) || ischar(va{i+1}))
