949b98f50a4f87ec73f06a39fd2770f2373b570e
Brent Townshend <bst@tc.com>
2021-04-17 00:45:14 -0700

    Added more options to pubfigure2

--- pubfigure2.m
+++ pubfigure2.m
@@ -2,7 +2,7 @@
 % Sets width, height in inches
 % Use width,height as the dimensions of the current axes in the plot
 function pubfigure2(filename,fnum,width,height,varargin)
-  defaults=struct('tickfontsize',[],'axisfontsize',[],'titlefontsize',[],'format','epsc2','markersize',[],'scale',1,'savedata',false,'minfontsize',10,'maxfontsize',14,'childfontsize',[],'units','inches','srcdata',[]);
+  defaults=struct('tickfontsize',[],'axisfontsize',[],'titlefontsize',[],'format','epsc2','markersize',[],'xmarkersizer',8,'scale',1,'savedata',false,'minfontsize',10,'maxfontsize',14,'childfontsize',[],'units','inches','srcdata',[],'font',[],'extra',[1,1]);
   args=processargs(defaults,varargin);
   if nargin<2
     fnum=gcf;
@@ -33,6 +33,10 @@ function pubfigure2(filename,fnum,width,height,varargin)
     args.axisfontsize=args.axisfontsize*args.scale;
     args.titlefontsize=args.titlefontsize*args.scale;
     args.childfontsize=args.childfontsize*args.scale;
+    if ~isempty(args.markersize)
+      args.markersize=args.markersize*args.scale;
+      args.xmarkersize=args.xmarkersize*args.scale;
+    end
   end
   
   
@@ -40,7 +44,9 @@ function pubfigure2(filename,fnum,width,height,varargin)
   %  set(gca,'Box','off');
   set(gcf,'PaperUnits',args.units);
   set(gcf,'Units',args.units);
-
+  if ~isempty(args.font)
+    set(gca,'FontName',args.font);
+  end
 
   % Walk through children to update any font or marker sizes
   axes=get(gcf,'children');
@@ -57,7 +63,7 @@ function pubfigure2(filename,fnum,width,height,varargin)
       for i=1:length(c)
         try
           if get(c(i),'Marker')=='x'
-            set(c(i),'MarkerSize',8);
+            set(c(i),'MarkerSize',args.xmarkersize);
           else
             set(c(i),'MarkerSize',args.markersize);
           end
@@ -82,6 +88,7 @@ function pubfigure2(filename,fnum,width,height,varargin)
   for i=1:10
     % Need to iterate as margins change slightly with figure resizing
     psn=get(gca,'Position');
+    %psn(3)=psn(3)*.9;   % Reduce right border to avoid clipping
     fprintf('Border fractions: %.3f %.3f\n', psn(3:end));
     fpos=get(gcf,'Position');
     borders=(1-(psn(3:4)+priorpsn(3:4))/2).*fpos(3:4);
@@ -90,16 +97,21 @@ function pubfigure2(filename,fnum,width,height,varargin)
     fheight=height+borders(2);
 
     set(gcf,'PaperPositionMode','manual');
-    set(gcf,'PaperPosition',[0 0 fwidth fheight]);
-    set(gcf,'PaperSize',[fwidth height])
+    set(gcf,'PaperPosition',[0 0 fwidth*args.extra(1) fheight*args.extra(2)]);  % Scale a bit to avoid clipping
+    set(gcf,'PaperSize',[fwidth*args.extra(1) height*args.extra(2)])
 
     % Resize display figure as well as print figure
     set(gcf,'Position',[fpos(1) fpos(2) fwidth fheight]);
     pause(0.1);  % Pause to cause figure update
   end
   
-  print(gcf,sprintf('-d%s',args.format),filename);
-  fprintf('Saved figure to %s with format %s\n', filename,args.format);
+  if strcmp(args.format,'vector')
+    exportgraphics(gcf,filename,'contenttype','vector');
+    fprintf('Exported vector graphics to %s\n', filename);
+  else
+    print(gcf,sprintf('-d%s',args.format),filename);
+    fprintf('Saved figure to %s with format %s\n', filename,args.format);
+  end
   if ~isempty(args.srcdata)
     % Explicit sourcedata in table form
     assert(istable(args.srcdata));
